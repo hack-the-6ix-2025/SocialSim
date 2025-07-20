5237cbdd52c1f65a2b1064e74a2f007c535b7d07
erinjhu <140906428+erinjhu@users.noreply.github.com>
2025-07-20 04:30:06 -0400

    access supabase image buckets

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: [
+      'kgnugzerbfacbfzebbcj.supabase.co', 
+    ],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
